07b357113c5430d0e55accdf0a59e545c0cf8c55
wuqitian <echo740@sjtu.edu.cn>
2023-02-22 14:43:27 +0800

    modify model name

--- GNNSafe/gnnsafe.py
+++ GNNSafe/gnnsafe.py
@@ -105,6 +105,9 @@ class GNNSafe(nn.Module):
             if args.use_prop: # use energy belief propagation
                 energy_in = self.propagation(energy_in, edge_index_in, args.K, args.alpha)[train_in_idx]
                 energy_out = self.propagation(energy_out, edge_index_out, args.K, args.alpha)[train_ood_idx]
+            else:
+                energy_in = energy_in[train_in_idx]
+                energy_out = energy_out[train_in_idx]
 
             # truncate to have the same length
             if energy_in.shape[0] != energy_out.shape[0]:
